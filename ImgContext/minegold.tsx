ffef87173d9cba67bd6133b8d42436e57d3c502e
Frank Collins <fwc3rd@gmail.com>
2023-08-08 11:14:04 -0400

    ImgContext [11:13am]

---
+++ ImgContext/minegold.tsx
@@ -0,0 +1,80 @@
+import React, { createContext, useContext, ReactNode, useState } from "react";
+
+// type the state which will be read-only 'string'
+type imgContextType = {
+    // first 6 images aren't captcha validating 
+    kiss: string;
+    signupsigns: string;
+    dynamite: string;
+    helmet: string;
+    barrier: string;
+    cactus: string;
+
+    // these 3 images have gold in them which means they do validate the captcha once clicked.
+    trophy: string;
+    gold: string;
+    cart: string;
+};
+
+// kiss, signupsigns, dynamite, helmet, barrier, cactus,
+//  trophy, gold, cart
+
+// define values which will remain static
+const imgDefaults: imgContextType = {
+    // no gold in the img. these images don't validate the captcha.
+    kiss: 'minegoldimg/kiss.png',
+    signupsigns: 'minegoldimg/signupsigns.png',
+    dynamite: 'minegoldimg/dynamite.png',
+    helmet: 'minegoldimg/helmet.png',
+    barrier: 'minegoldimg/barrier.png',
+    cactus: 'minegoldimg/cactus.png',
+
+    trophy: 'minegoldimg/trophy.png',
+    gold: 'minegoldimg/gold.png',
+    cart: 'minegoldimg/cart.png',
+
+};
+
+// createContext
+const ImgContext = createContext<imgContextType>(imgDefaults);
+
+export function useImage() {
+    return useContext(ImgContext);
+}
+
+type Props = {
+    children: ReactNode;
+};
+
+export function ImgProvider({ children }: Props) {    
+    const [kiss, setkiss] = useState<string>(`minegoldimg/kiss.png`);
+    const [signupsigns, setsignupsigns] = useState<string>(`minegoldimg/signupsigns.png`);
+    const [dynamite, setdynamite] = useState<string>(`minegoldimg/dynamite.png`);
+    const [helmet, sethelmet] = useState<string>(`minegoldimg/helmet.png`);
+    const [barrier, setbarrier] = useState<string>(`minegoldimg/barrier.png`);
+    const [cactus, setcactus] = useState<string>(`minegoldimg/cactus.png`);
+
+    const [trophy, settrophy] = useState<string>(`minegoldimg/trophy.png`);
+    const [gold, setgold] = useState<string>(`minegoldimg/gold.png`);
+    const [cart, setcart] = useState<string>(`minecartimg/cart.png`);
+        
+    const value = {
+        kiss,
+        signupsigns,
+        dynamite,
+        helmet,
+        barrier,
+        cactus,
+
+        trophy,
+        gold,
+        cart
+    };
+
+    return (
+        <>
+            <ImgContext.Provider value={value}>
+                {children}
+            </ImgContext.Provider>
+        </>
+    );}
